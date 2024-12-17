0c494bc1ff9536f290b17c776077f90be2670dd7
RyanOnTheInside <7623207+ryanontheinside@users.noreply.github.com>
2024-12-17 14:02:35 -0500

    String Control, more examples, demo

--- __init__.py
+++ __init__.py
@@ -1,15 +1,18 @@
-from .control_nodes import FloatControl, IntControl
+from .control_nodes import FloatControl, IntControl, StringControl
 from .quick_shape_mask import QuickShapeMask
 
 NODE_CLASS_MAPPINGS = {
     "FloatControl": FloatControl,
     "IntControl": IntControl,
+    "StringControl": StringControl,
     "QuickShapeMask": QuickShapeMask
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FloatControl": "Float Control (RyanOnTheInside)",
-    "IntControl": "Int Control (RyanOnTheInside)"
+    "IntControl": "Int Control (RyanOnTheInside)",
+    "StringControl": "String Control (RyanOnTheInside)",
+    "QuickShapeMask": "Quick Shape Mask (RyanOnTheInside)"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS'] 
