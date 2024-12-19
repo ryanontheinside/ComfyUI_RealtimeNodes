3c3b5a184e6f579b294f8f02beb9c805a0051c35
RyanOnTheInside <7623207+ryanontheinside@users.noreply.github.com>
2024-12-19 09:56:11 -0500

    abstracted state management, added sequence controls, updated examples

--- __init__.py
+++ __init__.py
@@ -1,10 +1,18 @@
-from .control_nodes import FloatControl, IntControl, StringControl
+from .controls.value_controls import FloatControl, IntControl, StringControl
+from .controls.sequence_controls import FloatSequence, IntSequence, StringSequence
+from .controls.utility_controls import FPSMonitor
 from .quick_shape_mask import QuickShapeMask
 
 NODE_CLASS_MAPPINGS = {
     "FloatControl": FloatControl,
     "IntControl": IntControl,
     "StringControl": StringControl,
+    "FloatSequence": FloatSequence,
+    "IntSequence": IntSequence,
+    "StringSequence": StringSequence,
+    "FPSMonitor": FPSMonitor,
+    #"IntervalControl": IntervalControl,
+    #"DeltaControl": DeltaControl,
     "QuickShapeMask": QuickShapeMask
 }
 
@@ -12,6 +20,12 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "FloatControl": "Float Control (RyanOnTheInside)",
     "IntControl": "Int Control (RyanOnTheInside)",
     "StringControl": "String Control (RyanOnTheInside)",
+    "FloatSequence": "Float Sequence (RyanOnTheInside)",
+    "IntSequence": "Int Sequence (RyanOnTheInside)",
+    "StringSequence": "String Sequence (RyanOnTheInside)",
+    "FPSMonitor": "FPS Monitor (RyanOnTheInside)",
+    #"IntervalControl": "Interval Control (RyanOnTheInside)",
+    #"DeltaControl": "Delta Control (RyanOnTheInside)",
     "QuickShapeMask": "Quick Shape Mask (RyanOnTheInside)"
 }
 
