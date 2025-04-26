55a8b8d2678397de4570f8be060ca0af3490c477
RyanOnTheInside <7623207+ryanontheinside@users.noreply.github.com>
2025-04-26 10:27:53 -0400

    always equal proxy

---
+++ src/utils/general.py
@@ -0,0 +1,13 @@
+"""
+General utilities for ComfyUI RealTimeNodes.
+"""
+
+class AlwaysEqualProxy(str):
+    """Helper class to allow connecting multiple specific input types to a single input node.
+       Originally borrowed from https://github.com/theUpsider/ComfyUI-Logic
+    """
+    def __eq__(self, _):
+        return True
+
+    def __ne__(self, _):
+        return False 
